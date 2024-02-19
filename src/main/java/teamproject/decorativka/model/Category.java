cff99fd3eb8b9dede75434daae68c2a3d1c5e881
Anton Babych <antonbabi13@gmail.com>
2024-02-19 17:17:32 +0200

    [MODEL] Add Product and Category model. add liquibase script. Add soft delete support to this model

---
+++ src/main/java/teamproject/decorativka/model/Category.java
@@ -0,0 +1,27 @@
+package teamproject.decorativka.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    @PreRemove
+    public void preRemove() {
+        this.deleted = true;
+    }
+}
