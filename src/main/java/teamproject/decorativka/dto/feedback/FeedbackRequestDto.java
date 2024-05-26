b892f119c69a2235da5e404408c1e4349aa21c24
AntonBabychP1T <antonbabi13@gmail.com>
2024-05-26 21:31:15 +0300

    [FEEDBACK] Add number field to FeedbackRequestDto

--- src/main/java/teamproject/decorativka/dto/feedback/FeedbackRequestDto.java
+++ src/main/java/teamproject/decorativka/dto/feedback/FeedbackRequestDto.java
@@ -2,14 +2,16 @@ package teamproject.decorativka.dto.feedback;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record FeedbackRequestDto(
         @NotNull
         String name,
         @Email
-        @NotNull
         String email,
         @NotNull
+        @Size(min = 10, max = 15)
+        String number,
         String comment
 ) {
 }
